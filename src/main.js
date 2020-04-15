dd3ff4ce2237b801fb025939279daf753b2df658
Dxxxxxxww <gotu2018@163.com>
2020-04-15 23:31:12 +0800

    增加 字体文件 by dxw

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import './styles/index.styl';
+import './styles/index.styl'
 
 Vue.config.productionTip = false
 
