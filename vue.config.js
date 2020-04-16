52628dcf1bd2948036bb0fa80c919fd508b62a47
Dxxxxxxww <gotu2018@163.com>
2020-04-16 18:52:21 +0800

    增加字体文件，postcssrc by dxw

--- vue.config.js
+++ vue.config.js
@@ -11,5 +11,34 @@ module.exports = {
       .set('@components', resolve('src/components'))
       .set('@assets', resolve('src/assets'))
       .set('styles', resolve('styles'))
+    // 配置引入字体
+    // config.module.rule([
+    //   {
+    //     test: /\.(woff2?|eot|ttf|otf)(\?.*)$/,
+    //     loder: 'url-loader',
+    //     options: {
+    //       limit: 10000
+    //     }
+    //   }
+    // ])
   }
+  // 配置引入字体
+  // configureWebpack: {
+  //   plugins: [],
+  //   module: {
+  //     rules: [
+  //       {
+  //         test: /\.(woff2?|eot|ttf|otf)(\?.*)$/,
+  //         use: [
+  //           {
+  //             loader: 'url-loader',
+  //             options: {
+  //               limit: 10000
+  //             }
+  //           }
+  //         ]
+  //       }
+  //     ]
+  //   }
+  // }
 }
